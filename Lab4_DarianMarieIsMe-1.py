ceae4a6eac105184200e9a82622a284550009565
Darian Bruce <darianmariebruce@gmail.com>
2026-02-06 18:59:40 -0500

    user input created

--- Lab4_DarianMarieIsMe-1.py
+++ Lab4_DarianMarieIsMe-1.py
@@ -3,3 +3,7 @@ Darian Marie Bruce
 This program simulates dealing a hand of cards to
 the user
 02/06/2026'''
+
+print("This program will deal you a hand of random cards.")
+
+users_cards = int(input("Please enter the number of cards you would like to be dealt: "))
