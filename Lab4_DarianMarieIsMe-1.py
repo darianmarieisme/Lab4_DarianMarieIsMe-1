3a9ef4d8bd4a57afcfe8fe548a726561b96e9970
Darian Bruce <darianmariebruce@gmail.com>
2026-02-06 20:09:22 -0500

    added another while loop to check input is valid so program doesn't break due to out of bounds numbers

--- Lab4_DarianMarieIsMe-1.py
+++ Lab4_DarianMarieIsMe-1.py
@@ -8,7 +8,14 @@ import random
 
 print("This program will deal you a hand of random cards.")
 
-users_cards = int(input("Please enter the number of cards you would like to be dealt: "))
+#check that input is valid
+
+while True:
+    users_cards = int(input("How many cards would you like to be dealt? Please input a valid amount. (1-52): "))
+    if 1 <= users_cards <= 52:
+        break
+    else:
+        print("Invalid input. Please enter a number between 1 and 52.")
 
 #Per assignment instructions, cards are represented and abbreviated in a shorthand format
 
@@ -16,10 +23,14 @@ card_values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 card_suits = ["h", "d", "c", "s"]
 
+#create a list to hold the user's hand of cards
+
 hand = []
 
 
 print(f"You have requested {users_cards} cards. Here is your hand:")
+
+# Logic to print hand of cards
 
 while len(hand) < users_cards:
     card = random.choice(card_values) + random.choice(card_suits)
